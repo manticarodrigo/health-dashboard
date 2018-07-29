c7867e82d10491b2b900922a8091ee9f1cd0534f
manticarodrigo <manticarodrigo@gmail.com>
2018-07-29 10:52:59 -0400

    github pages and hash router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import "assets/css/main.css";
 
